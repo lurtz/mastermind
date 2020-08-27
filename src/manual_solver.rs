13508cb124180f0a0f15bd42bace9dbc80812c59
Lutz <derlurtz@gmail.com>
2020-08-27 10:27:48 +0200

    make manual solver a bit more testable

--- src/manual_solver.rs
+++ src/manual_solver.rs
@@ -4,15 +4,11 @@ use crate::mastermind_state::{get_guess_from_string, Values};
 use crate::util::CURSOR_UP;
 use std::io::stdin;
 
-fn get_guess() -> Result<Values, std::io::Error> {
-    let mut buf = String::new();
-    stdin().read_line(&mut buf)?;
-    Ok(get_guess_from_string(buf))
-}
+type InputFn = fn() -> Result<Values, std::io::Error>;
 
-pub fn solve(mm: &mut Mastermind) -> Values {
+fn solve_with_input(mm: &mut Mastermind, input: InputFn) -> Values {
     Colors::show_number_mapping();
-    let mut guess = get_guess().unwrap();
+    let mut guess = input().unwrap();
     let mut solved = false;
     while !solved {
         print!("{}", CURSOR_UP);
@@ -20,19 +16,46 @@ pub fn solve(mm: &mut Mastermind) -> Values {
         if GuessStatus::Success == status {
             solved = true;
         } else {
-            guess = get_guess().unwrap();
+            guess = input().unwrap();
         }
     }
     guess
 }
 
+fn get_guess() -> Result<Values, std::io::Error> {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf)?;
+    Ok(get_guess_from_string(buf))
+}
+
+pub fn solve(mm: &mut Mastermind) -> Values {
+    solve_with_input(mm, get_guess)
+}
+
 #[cfg(test)]
 mod test {
+    use crate::colors::Colors;
     use crate::manual_solver::solve;
+    use crate::manual_solver::solve_with_input;
+    use crate::mastermind::Mastermind;
+    use crate::mastermind_state::{get_guess_from_string, MastermindState, Values};
     use crate::solver::SolverFn;
 
     #[test]
     fn solve_has_correct_type() {
         let _solvefn: SolverFn = solve;
+    }
+
+    #[test]
+    fn solve_with_correct_guess() {
+        let return_black = || -> Result<Values, std::io::Error> {
+            Ok(get_guess_from_string(String::from("5555")))
+        };
+        let values = [Colors::Black; 4];
+        let mut mm = Mastermind::new_with_state(values);
+        let solution = solve_with_input(&mut mm, return_black);
+        let pattern = mm.get_initial();
+        assert!(pattern.are_values_equal(&solution));
+        assert!(MastermindState::new_initial(values).are_values_equal(&solution));
     }
 }
